4064bc27054c8e32fc6ba74e985e2e37e62e6a60
bipika22 <bipika@technologyspot.com.np>
2024-06-18 06:48:36 +0545

    modified:id

--- src/app/todo/todo-list.tsx
+++ src/app/todo/todo-list.tsx
@@ -1,60 +1,23 @@
-import { useState } from "react";
 import TodoItem from "./todo-item";
 
 
-const DEFAULT_TODO_LIST = [
-  {
-    id: 1,
-    title: "Go to Office",
-    description: "Please reach before 9",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "Drink Water",
-    description: "Every 4hr",
-    completed: true,
-  },
-  {
-    id: 3,
-    title: "Test Water",
-    description: "Every 4hr",
-    completed: false,
-  },
-];
-export default function Todolist() {
-  const [todoList, setTodoList] = useState(DEFAULT_TODO_LIST);
-  const toggleCompleted = (id) => {
-    const newTodos = todoList.map((todo) => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      }
-      return todo
-    })
-    setTodoList(newTodos)
-  }
-  const removeTodo =(id)=>{
-const newTodos =todoList.filter(item=>item.id!==id)
 
-setTodoList(newTodos)
-  }
+export default function Todolist(props) {
+const {todoList, toggleCompleted, removeTodo}  = props
 
   return (
     <ul>
       {todoList.map((item) => {
+        const {id,title,description,completed} = item
         return (
           <TodoItem
-            key={`todo-${item.id}`}
-            text={item.title}
-            description={item.description}
-            completed={item.completed}
-            id={item.id}
+            key={`todo-${id}`}
+            text={title}
+            description={description}
+            completed={completed}
+            id={id}
             toggleCompleted={toggleCompleted}
-            removeTodo={removeTodo}
-          />
+            removeTodo={removeTodo} />
         ); 
       })}
     </ul>
--- src/app/todo/todo.tsx
+++ src/app/todo/todo.tsx
@@ -1,15 +1,45 @@
-
-import Button from "../../components/button";
 import Search from "./search";
 import TodoList from "./todo-list";
+import TodoForm from "./todo-form";
+import { useState } from "react";
+
 
 export default function Todo(){
+    const [todoList, setTodoList] = useState([]);
+  const toggleCompleted = (id) => {
+    const newTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo
+    })
+    setTodoList(newTodos)
+  }
+  const removeTodo =(id)=>{
+const newTodos =todoList.filter(item=>item.id!==id)
+
+setTodoList(newTodos)
+  }
+const addTodo =(todo)=>{
+  const newTodos = [...todoList,todo]
+  setTodoList(newTodos)
+}
     return (
         <div>
         <h1>Todo App</h1>
         <Search/>
-        <TodoList/>
-        <Button title="Add" />
+        <TodoForm addTodo={addTodo}/>
+        <TodoList 
+                todoList={todoList}
+                toggleCompleted={toggleCompleted}
+                removeTodo={removeTodo}
+        
+        />
+
+        {/* <Button title="Add Todo"/> */}
 
         </div>
     )
