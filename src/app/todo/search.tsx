57e9db39e59e3c8a8c1c10119481c4c61900f66e
bipika22 <bipika@technologyspot.com.np>
2024-06-18 07:24:36 +0545

    saerh functionality addedc

--- src/app/todo/search.tsx
+++ src/app/todo/search.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+// import { useState } from "react";
 //function based
 //class based
 // uncontrolled component
 //controlled component
 
-export default function Search() {
-  const [search,setSearch] = useState('');
-  const handleChange = (e) =>{
-    console.log(e.target.value)
-    setSearch(e.target.value)
-}
+export default function Search(props) {
+  const { search, handleChange } = props;
   return (
     <div>
-      <input type="text" className="border rounded py-2 px-3" value={search} onChange={handleChange}/>
-      <h1>Search Result: {search}</h1>
+      <input
+        type="text"
+        className="border rounded py-2 px-3"
+        value={search}
+        onChange={handleChange}
+        placeholder="Search"
+      />
+      <h1>Search Result {search}</h1>
     </div>
   );
 }
